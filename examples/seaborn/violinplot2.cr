0f4e2505f05cbee57831b0179f711689ab1fb049
kojix2 <2xijok@gmail.com>
2025-02-02 19:25:13 +0900

    Add example

---
+++ examples/seaborn/violinplot2.cr
@@ -0,0 +1,16 @@
+require "../../src/crython"
+
+Crython.embed_python do
+  sns = Crython.import("seaborn")
+  sns.set_theme
+
+  seaice = sns.load_dataset("seaice")
+  seaice["Decade"] = seaice["Date"].dt.year.round(-1)
+  sns.violinplot(seaice,
+    x: "Extent", y: "Decade",
+    orient: "y", fill: false
+  )
+
+  plt = Crython.import("matplotlib.pyplot")
+  plt.show
+end
