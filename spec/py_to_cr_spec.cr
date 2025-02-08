1613d6b3ef7beb44face17f5c6edf2c999afe1e7
kojix2 <2xijok@gmail.com>
2025-02-08 20:54:18 +0900

    Add support for number conversion from Python to Crystal types

--- spec/py_to_cr_spec.cr
+++ spec/py_to_cr_spec.cr
@@ -3,9 +3,9 @@ require "./spec_helper"
 describe Number do
   it "long converts to Crystal" do
     Crython.session do
-      py_long = 256.to_py
+      py_long = 255.to_py
       py_long.should be_a(Crython::PyObject)
-      py_long.to_cr.should eq(256)
+      py_long.to_cr.should eq(255)
     end
   end
 
@@ -22,6 +22,90 @@ describe Number do
       py_str = "hello".to_py
       py_str.should be_a(Crython::PyObject)
       py_str.to_cr.should eq("hello")
+    end
+  end
+
+  it "UInt8 converts from Python" do
+    Crython.session do
+      pyobject = 255.to_py
+      UInt8.new(pyobject).should eq(255)
+    end
+  end
+
+  it "Int8 converts from Python" do
+    Crython.session do
+      pyobject = (-128).to_py
+      Int8.new(pyobject).should eq(-128)
+    end
+  end
+
+  it "UInt16 converts from Python" do
+    Crython.session do
+      pyobject = 65535.to_py
+      UInt16.new(pyobject).should eq(65535)
+    end
+  end
+
+  it "Int16 converts from Python" do
+    Crython.session do
+      pyobject = (-32768).to_py
+      Int16.new(pyobject).should eq(-32768)
+    end
+  end
+
+  it "UInt32 converts from Python" do
+    Crython.session do
+      pyobject = 4294967295.to_py
+      UInt32.new(pyobject).should eq(4294967295)
+    end
+  end
+
+  it "Int32 converts from Python" do
+    Crython.session do
+      pyobject = (-2147483648).to_py
+      Int32.new(pyobject).should eq(-2147483648)
+    end
+  end
+
+  it "UInt64 converts from Python" do
+    Crython.session do
+      pyobject = 18446744073709551615_u64.to_py
+      UInt64.new(pyobject).should eq(18446744073709551615_u64)
+    end
+  end
+
+  it "Int64 converts from Python" do
+    Crython.session do
+      pyobject = (-9223372036854775808).to_py
+      Int64.new(pyobject).should eq(-9223372036854775808)
+    end
+  end
+
+  it "UInt128 converts from Python" do
+    Crython.session do
+      pyobject = 340282366920938463463374607431768211455_u128.to_py
+      UInt128.new(pyobject).should eq(340282366920938463463374607431768211455_u128)
+    end
+  end
+
+  it "Int128 converts from Python" do
+    Crython.session do
+      pyobject = (-170141183460469231731687303715884105728_i128).to_py
+      Int128.new(pyobject).should eq(-170141183460469231731687303715884105728_i128)
+    end
+  end
+
+  it "Float32 converts from Python" do
+    Crython.session do
+      pyobject = 3.14.to_py
+      Float32.new(pyobject).should eq(3.14.to_f32)
+    end
+  end
+
+  it "Float64 converts from Python" do
+    Crython.session do
+      pyobject = 3.14.to_py
+      Float64.new(pyobject).should eq(3.14)
     end
   end
 end
--- src/crython/cr2py/number.cr
+++ src/crython/cr2py/number.cr
@@ -57,12 +57,20 @@ struct UInt8
     ptr = Crython::LibPython.long_from_unsigned_long(self)
     Crython::PyObject.new(ptr, need_decref: true)
   end
+
+  def self.new(pyobject : Crython::PyObject) : UInt8
+    Crython::LibPython.long_as_unsigned_long(pyobject).to_u8
+  end
 end
 
 struct Int8
   def to_py : Crython::PyObject
     ptr = Crython::LibPython.long_from_long(self)
     Crython::PyObject.new(ptr, need_decref: true)
+  end
+
+  def self.new(pyobject : Crython::PyObject) : Int8
+    Crython::LibPython.long_as_long(pyobject).to_i8
   end
 end
 
@@ -71,12 +79,20 @@ struct UInt16
     ptr = Crython::LibPython.long_from_unsigned_long(self)
     Crython::PyObject.new(ptr, need_decref: true)
   end
+
+  def self.new(pyobject : Crython::PyObject) : UInt16
+    Crython::LibPython.long_as_unsigned_long(pyobject).to_u16
+  end
 end
 
 struct Int16
   def to_py : Crython::PyObject
     ptr = Crython::LibPython.long_from_long(self)
     Crython::PyObject.new(ptr, need_decref: true)
+  end
+
+  def self.new(pyobject : Crython::PyObject) : Int16
+    Crython::LibPython.long_as_long(pyobject).to_i16
   end
 end
 
@@ -85,12 +101,20 @@ struct UInt32
     ptr = Crython::LibPython.long_from_unsigned_long(self)
     Crython::PyObject.new(ptr, need_decref: true)
   end
+
+  def self.new(pyobject : Crython::PyObject) : UInt32
+    Crython::LibPython.long_as_unsigned_long(pyobject).to_u32
+  end
 end
 
 struct Int32
   def to_py : Crython::PyObject
     ptr = Crython::LibPython.long_from_long(self)
     Crython::PyObject.new(ptr, need_decref: true)
+  end
+
+  def self.new(pyobject : Crython::PyObject) : Int32
+    Crython::LibPython.long_as_long(pyobject).to_i32
   end
 end
 
@@ -99,12 +123,20 @@ struct UInt64
     ptr = Crython::LibPython.long_from_unsigned_long_long(self)
     Crython::PyObject.new(ptr, need_decref: true)
   end
+
+  def self.new(pyobject : Crython::PyObject) : UInt64
+    Crython::LibPython.long_as_unsigned_long_long(pyobject).to_u64
+  end
 end
 
 struct Int64
   def to_py : Crython::PyObject
     ptr = Crython::LibPython.long_from_long_long(self)
     Crython::PyObject.new(ptr, need_decref: true)
+  end
+
+  def self.new(pyobject : Crython::PyObject) : Int64
+    Crython::LibPython.long_as_long_long(pyobject).to_i64
   end
 end
 
@@ -115,6 +147,13 @@ struct UInt128
     ptr = Crython::LibPython.long_from_unicode_object(py_str, 10)
     Crython::PyObject.new(ptr, need_decref: true)
   end
+
+  def self.new(pyobject : Crython::PyObject) : UInt128
+    py_str = Crython::LibPython.object_str(pyobject)
+    cstr = Crython::LibPython.unicode_as_utf8(py_str)
+    str_repr = String.new(cstr)
+    str_repr.to_u128
+  end
 end
 
 struct Int128
@@ -124,6 +163,13 @@ struct Int128
     ptr = Crython::LibPython.long_from_unicode_object(py_str, 10)
     Crython::PyObject.new(ptr, need_decref: true)
   end
+
+  def self.new(pyobject : Crython::PyObject) : Int128
+    py_str = Crython::LibPython.object_str(pyobject)
+    cstr = Crython::LibPython.unicode_as_utf8(py_str)
+    str_repr = String.new(cstr)
+    str_repr.to_i128
+  end
 end
 
 struct Float32
@@ -131,11 +177,19 @@ struct Float32
     ptr = Crython::LibPython.build_value("f", self)
     Crython::PyObject.new(ptr, need_decref: true)
   end
+
+  def self.new(pyobject : Crython::PyObject) : Float32
+    Crython::LibPython.float_as_double(pyobject).to_f32
+  end
 end
 
 struct Float64
   def to_py : Crython::PyObject
     ptr = Crython::LibPython.build_value("d", self)
     Crython::PyObject.new(ptr, need_decref: true)
+  end
+
+  def self.new(pyobject : Crython::PyObject) : Float64
+    Crython::LibPython.float_as_double(pyobject)
   end
 end
