30d044f621db63956c124d224526e1c4e0497a05
kojix2 <2xijok@gmail.com>
2025-02-09 01:03:58 +0900

    Add Stable ABI functions for set and frozenset operations

--- src/crython/libpython.cr
+++ src/crython/libpython.cr
@@ -293,5 +293,17 @@ module Crython
     fun iter_check = PyIter_Check(o : PyObject) : Int
     fun aiter_check = PyAIter_Check(o : PyObject) : Int
     fun iter_next = PyIter_Next(o : PyObject) : PyObject
+
+    # Set and Frozenset
+    fun set_type = PySet_Type : PyObject
+    fun frozenset_type = PyFrozenSet_Type : PyObject
+    fun set_new = PySet_New(iterable : PyObject) : PyObject
+    fun frozenset_new = PyFrozenSet_New(iterable : PyObject) : PyObject
+    fun set_size = PySet_Size(anyset : PyObject) : Py_ssize_t
+    fun set_contains = PySet_Contains(anyset : PyObject, key : PyObject) : Int
+    fun set_add = PySet_Add(set : PyObject, key : PyObject) : Int
+    fun set_discard = PySet_Discard(set : PyObject, key : PyObject) : Int
+    fun set_pop = PySet_Pop(set : PyObject) : PyObject
+    fun set_clear = PySet_Clear(set : PyObject) : Int
   end
 end
